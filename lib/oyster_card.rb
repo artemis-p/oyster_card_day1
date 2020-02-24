52f8efe1d3d1d2cee35f0e9be73dc387ea5f0f7f
Orion Reed <orionreedt@gmail.com>
2020-02-24 16:30:03 +0000

    steps 6-7 (whoops)

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,7 +1,18 @@
 class OysterCard
-attr_reader :balance
+  MAX_BALANCE = 90
+  attr_reader :balance
 
   def initialize
     @balance = 0
-  end 
+  end
+
+  def top_up(amount)
+    raise "Exceeded maximum balance of £#{MAX_BALANCE}" if @balance + amount > MAX_BALANCE
+    @balance += amount
+  end
+
+  def deduct(amount)
+    raise "Not enough money, current balance is £#{@balance}" if @balance - amount < 0
+    @balance -= amount
+  end
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,5 +1,21 @@
 require 'oyster_card'
 
 describe OysterCard do
-  it { is_expected.to respond_to(:balance) }
+  it { is_expected.to(respond_to(:balance, :top_up)) }
+  it 'has default balance of 0' do
+    expect(subject.balance).to(eq(0))
+  end
+  it 'adds to balance when topped up' do
+    subject.top_up(5)
+    expect(subject.balance).to(eq(5))
+  end
+  it 'throws an error when exceeding max balance' do
+    expect { OysterCard.new.top_up(OysterCard::MAX_BALANCE + 1) }.to(raise_error("Exceeded maximum balance of £#{OysterCard::MAX_BALANCE}"))
+  end
+  subject { OysterCard.new }
+  it 'deducts balance from card' do
+    subject.top_up(15)
+    subject.deduct(5)
+    expect { subject.deduct(5) }.to(change { subject.balance }.by(-5))
+  end
 end
